ca32b64e51342612a35e1a798b3e99c5c6fb0260
Mike <mike.alibo@bitcontroltech.com>
2020-08-17 10:00:42 +0800

    Added Navigation Bar

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def blog
+  end
+
+  def portfolio
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get blog" do
+    get pages_blog_url
+    assert_response :success
+  end
+
+  test "should get portfolio" do
+    get pages_portfolio_url
+    assert_response :success
+  end
+
+end
