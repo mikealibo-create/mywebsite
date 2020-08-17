21195e6ec6b8e32fa257482fcc0f3bc476eb081d
Mike <mike.alibo@bitcontroltech.com>
2020-08-17 17:59:08 +0800

    Fixed the home page content

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'blog', to: 'pages#blog'
   get 'portfolio', to: 'pages#portfolio'
+  get 'contact', to: 'pages#contact'
   root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
