43f075e1afc464bab961e5db281aadb5a2011eec
Mike <mike.alibo@bitcontroltech.com>
2020-08-27 15:53:31 +0800

    hoverable blog actions

--- app/javascript/packs/blogs.js
+++ app/javascript/packs/blogs.js
@@ -1,18 +1,19 @@
 
 $( document ).on('turbolinks:load', function() {
 
-  $('#imgInp').on('change', function(){
+  $('#blogImage').on('change', function(){
     previewFile()
   })
 
   function previewFile() {
-    const preview = document.querySelector('.img-prev');
+    const preview = document.querySelector('.blog-img-prev');
     const file = document.querySelector('input[type=file]').files[0];
     const reader = new FileReader();
   
     reader.addEventListener("load", function () {
       // convert image file to base64 string
       preview.src = reader.result;
+      preview.style.display = "block";
     }, false);
   
     if (file) {
